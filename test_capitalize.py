62297b8f8a96692e1d123cee3a30939c285e8c1e
Trent Williams <trent@trentkels.com>
2020-09-17 18:33:25 +1200

    trent added

--- test_capitalize.py
+++ test_capitalize.py
@@ -5,6 +5,7 @@ from capitalize import capital_case, square
 def test_capital_case():
     assert capital_case('semaphore') == 'Semaphore'
     assert capital_case('trent') == 'Trent'
+    assert capital_case('Hello') == 'hello'
 
 
 @pytest.mark.parametrize("data, expected", [(5, 25),(2, 4), (9, 81), (-3, 9)])
